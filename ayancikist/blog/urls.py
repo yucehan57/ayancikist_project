ec3c91e6a2a0bdb61858d8c9d94b713c89039d1b
yucehan.kucukmotor <ychnkckmtr@gmail.com>
2019-05-23 17:25:11 +0300

    Post model created

--- ayancikist/blog/urls.py
+++ ayancikist/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blog/', PostListView.as_view(), name='blog-view'),
 
 ]
--- ayancikist/blog/views.py
+++ ayancikist/blog/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Post
+from django.utils import timezone
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
 def index(request):
-    return render(request, 'blog/index.html', {})
+    # List published posts on home page
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'blog/index.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/posts.html'
+
+    def get_queryset(self):
+        # Filter by 'published_date' field of Post model
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+class PostDetailView(DetailView):
+    model = Post
