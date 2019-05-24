63ad0442946c50e2cded1bd3fb702fc38d9210a0
yucehan.kucukmotor <ychnkckmtr@gmail.com>
2019-05-24 20:21:32 +0300

    user authentication

--- ayancikist/accounts/views.py
+++ ayancikist/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages
+from django.contrib import messages, auth
 
 def register(request):
     if request.method == 'POST':
@@ -35,3 +35,20 @@ def register(request):
             return redirect('register')
     else:
         return render(request, 'accounts/register.html')
+
+def login(request):
+    if request.method == 'POST':
+        # Login Logic
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('blog-view')
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        return render(request, 'accounts/login.html')
